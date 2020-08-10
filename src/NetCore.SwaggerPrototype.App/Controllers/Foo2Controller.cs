4ea600846b9752bda701f31693f127adabc7314a
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-10 18:30:58 +0300

    Add Foo v1 and v2

---
+++ src/NetCore.SwaggerPrototype.App/Controllers/Foo2Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.SwaggerPrototype.App.Controllers
+{
+    ////[Authorize]
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}")]
+    [Produces("application/json")]
+    public sealed class Foo2Controller : ControllerBase
+    {
+        [HttpGet]
+        [Route("foos")]
+        public IActionResult Test()
+        {
+            return Ok("Response from api/v2/foos");
+        }
+    }
+}
---
+++ src/NetCore.SwaggerPrototype.App/Controllers/FooController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.SwaggerPrototype.App.Controllers
+{
+    ////[Authorize]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}")]
+    [Produces("application/json")]
+    public sealed class FooController : ControllerBase
+    {
+        [HttpGet]
+        [Route("foos")]
+        public IActionResult Test()
+        {
+            return Ok("Response from api/v1/foos");
+        }
+    }
+}
